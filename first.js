fc5f4721d5f4d92a25cb456d3ae1915ddff7712e
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-07-25 06:54:19 +0000

    finish 2-4

--- first.js
+++ first.js
@@ -1,3 +1,5 @@
+// JavaScriptの基本構文は「オブジェクト.メソッド('パラメター')」
 // window.alert('はじめてのJavaScript');
-// 冒頭の"window"は省略可能
+// 冒頭の"window"は省略可能、オブジェクトで省略できるのはwindowのみ
+// 要CHECK「エスケープ表記」＝「エスケープシーケンス」
 alert('はじめてのJavaScript');
